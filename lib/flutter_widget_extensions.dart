790db47b1fcf3f75e7fe18cb1d8983fc2031d4b3
Fabian Mendez <32204060+fabianMendez@users.noreply.github.com>
2021-02-15 19:27:47 -0500

    first commit

---
+++ lib/flutter_widget_extensions.dart
@@ -0,0 +1,62 @@
+library flutter_widget_extensions;
+
+import 'package:flutter/widgets.dart';
+
+extension WidgetExtensions on Widget {
+  Widget withConstraints({
+    double minWidth = 0.0,
+    double maxWidth = double.infinity,
+    double minHeight = 0.0,
+    double maxHeight = double.infinity,
+  }) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minWidth: minWidth,
+        maxWidth: maxWidth,
+        minHeight: minHeight,
+        maxHeight: maxHeight,
+      ),
+      child: this,
+    );
+  }
+
+  Widget withAlignment({
+    Alignment alignment = Alignment.center,
+    double widthFactor,
+    double heightFactor,
+  }) {
+    return Align(
+      child: this,
+      alignment: alignment,
+      widthFactor: widthFactor,
+      heightFactor: heightFactor,
+    );
+  }
+
+  Widget withPadding([EdgeInsets padding]) {
+    return Padding(padding: padding ?? const EdgeInsets.all(20), child: this);
+  }
+
+  Widget withDecoration(Decoration decoration) {
+    return DecoratedBox(decoration: decoration, child: this);
+  }
+
+  Widget withSize({double width, double height}) {
+    return SizedBox(width: width, height: height, child: this);
+  }
+
+  Widget expanded({int flex = 1}) {
+    return Expanded(child: this, flex: flex);
+  }
+
+  Widget centered({
+    double widthFactor,
+    double heightFactor,
+  }) {
+    return Center(
+      widthFactor: widthFactor,
+      heightFactor: heightFactor,
+      child: this,
+    );
+  }
+}
